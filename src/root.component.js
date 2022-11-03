f0aa640c696754cd53aec3bcd044384913770b1d
Sy Duy <duycs@outlook.com>
2022-11-03 16:47:23 +0700

    init

---
+++ src/root.component.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./root.component.css";
+
+export default function Root() {
+  return (
+    <div className="container1">
+      <p>App developed by Team 1</p>
+    </div>
+  );
+}
